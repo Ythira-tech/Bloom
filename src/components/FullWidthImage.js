887b91a3db4b618df088ab5cfe03fad4090a3eb4
vivian wambugu <ythirav@gmail.com>
2025-08-06 20:51:38 +0300

    Added fullwidthimage.js

---
+++ src/components/FullWidthImage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FullWidthImage.css';
+
+function FullWidthImage() {
+  return (
+    <div className="fullwidth-image-container">
+      <img
+        src="https://images.pexels.com/photos/9630216/pexels-photo-9630216.jpeg"
+        alt="Nature background"
+        className="fullwidth-image"
+      />
+     </div>
+  );
+}
+
+export default FullWidthImage;
